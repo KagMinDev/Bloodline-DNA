5fcb9c5cd824f54622e3a8e32b476dd5e2bd9b70
KagMin <phankangmin@gmail.com>
2025-06-10 10:55:30 +0700

    auth context check login

--- FE/MobileApp/Bloodline-DNA-App/App.tsx
+++ FE/MobileApp/Bloodline-DNA-App/App.tsx
@@ -1,15 +1,18 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import AppRouter from './src/navigations/appRouter';
+import { AuthProvider } from "@/context/auth/AuthContext";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import AppRouter from "./src/navigations/appRouter";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <AppRouter />
-        <StatusBar style="auto" />
-      </NavigationContainer>
+      <AuthProvider>
+        <NavigationContainer>
+          <AppRouter />
+          <StatusBar style="auto" />
+        </NavigationContainer>
+      </AuthProvider>
     </SafeAreaProvider>
   );
 }
