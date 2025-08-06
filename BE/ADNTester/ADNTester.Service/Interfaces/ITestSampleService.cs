43711e4c6f08e0162a720f29eab595a5f7e6d565
ducvl <Ducvldse172482@fpt.edu.vn>
2025-08-06 16:32:43 +0700

    Update GetTestSampleByKitId to return all samples for a kitId

--- BE/ADNTester/ADNTester.Service/Interfaces/ITestSampleService.cs
+++ BE/ADNTester/ADNTester.Service/Interfaces/ITestSampleService.cs
@@ -13,7 +13,7 @@ namespace ADNTester.Service.Interfaces
         Task<bool> UpdateAsync(UpdateTestSampleDto dto);
         Task<bool> DeleteAsync(string id);
         Task<IEnumerable<TestSampleDetailDto>> GetTestSampleByUserId(string userId);
-        Task<TestSampleDetailDto> GetTestSampleByKitId(string kitId);
+        Task<IEnumerable<TestSampleDetailDto>> GetTestSampleByKitId(string kitId);
         Task<string> CreateSampleFromClientAsync(CreateTestSampleFromClientDto dto);
     }
 } 
